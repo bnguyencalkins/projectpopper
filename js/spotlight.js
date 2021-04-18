5c05bc0b7d927a050a778b26dc83fbf19c4426b7
bnguyencalkins <72314555+bnguyencalkins@users.noreply.github.com>
2021-04-18 18:37:23 -0500

    Add files via upload

--- js/spotlight.js
+++ js/spotlight.js
@@ -11,19 +11,48 @@ function index() {
 
 }
 
-var placeholdingarray = {
+const logLink = 'https://docs.google.com/spreadsheets/d/1Tnnik4Ej7NycIFd-F6RrMjHqBUp3fzXoBRGx2PqEc9E/export?format=csv&grid=0';
 
+let result;
+/* Papa Parsing LOG file */
+const csvData = Papa.parse(logLink, {
+    download: true,
+    header: true,
+    skipEmptyLines: 'greedy',
+    delimiter: "",	// auto-detect
+    newline: "",	// auto-detect
+    qouteChar: '"',
+    escapeChar: '"',
+    transformHeader: name => {
+        switch(name) {
+          case 'Timestamp': { return 'Date'; break; }
+          case 'What is your name?': { return 'Author'; break; }
+          case 'What is the Title of your Project?': { return 'Title'; break; }
+          case 'What field of study does your project include? [Choices]': { return 'Field'; break; }
+          case 'What\'s a short description of your project?': { return 'Overview'; break; }
+          case 'Would you consider your project successful?': { return 'Success'; break; }
+          case 'Is there anything you would have done differently? If so, what?': { return 'Changes'; break; }
+        }
+      },
+    complete: function(result) {
+        console.log(result);
+        data = result.data;
+        makeCards(data);
+    }
+});
+
+function makeCards(data) {
+    for (i = 0; i < data.length; i++) {
+        var title = data[i].Title;
+        var author = data[i].Author;
+        var date = data[i].Date;
+        var field = data[i].Field;
+        var overview = data[i].Overview;
+        var success = data[i].Success;
+        var changes = data[i].Changes;
+
+
+        document.getElementById('holder').innerHTML = title + '<br>' + overview;
+    }
 }
 
-for (var i = 1; i < 10; i++) {
-    var projectCol = document.createElement("div");
-    var current = document.createTextNode(i);
-    projectCol.classList.add("col-md-4", "col-xs-6");
-
-/*Create the card within the column, later to be added to the column */
-    var projectCard = document.createElement("button");
-
-    projectCard.classList.add("card", "button", "placeholder");
-    projectCol.appendChild(projectCard);
-    document.getElementById("holder").appendChild(projectCol);
-}
